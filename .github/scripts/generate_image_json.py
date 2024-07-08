797c1a78eb68d5a538a836eb061701a580a0679a
tutuh <63507813+tutuh@users.noreply.github.com>
2024-07-08 16:23:06 +0800

    Update generate_image_json.py

--- .github/scripts/generate_image_json.py
+++ .github/scripts/generate_image_json.py
@@ -4,8 +4,8 @@ import json
 def generate_json():
     image_folder = 'iconSet'
     json_data = {
-        "name": "图标订阅",
-        "description": "收集一些自己脚本用到的图标",
+        "name": "IconSet",
+        "description": "Icon Set for Surge",
         "icons": []
     }
 
